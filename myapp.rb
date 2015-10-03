c5da08a51a854c3b60efd25bab0c56b9afca34d4
Marvin Laucher <marv070@gmail.com>
2015-10-03 12:07:49 -0400

    update using two paramiters with addition

--- myapp.rb
+++ myapp.rb
@@ -11,3 +11,8 @@ end
 get '/hello/:name' do
 	"Hello #{params['name']}"
 end
+
+get '/addition/:var1/:var2' do |input1, input2|
+     sum = input1.to_i + input2.to_i 
+	"addition of var1 + var2 = " + sum.to_s
+end
