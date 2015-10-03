f2f5fce0e4124b668326f42ecf3c5c5fd47eaf64
Marvin Laucher <marv070@gmail.com>
2015-10-03 13:24:49 -0400

    new text box

--- myapp.rb
+++ myapp.rb
@@ -21,3 +21,7 @@ get '/multiply/:var1/:var2/:var3' do |name, input2, input3|
 	product = input2.to_i * input3.to_i
 	"Hello #{name} mulitplication of var2 * var3 = "  + product.to_s
 end
+
+get '/details' do
+	erb :username
+end
