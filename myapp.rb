866de8dd3ea71f74d58075a5ad17278ef936f993
Marvin Laucher <marv070@gmail.com>
2015-10-03 16:32:45 -0400

    update to see how old and 3 fav num

--- myapp.rb
+++ myapp.rb
@@ -27,9 +27,25 @@ get '/details' do
 end
 post '/details' do
 	erb :age, :locals => {:name => params['user']}
+
 end
 
 
 post '/age' do
+  if params[:howold].to_i < 60
+   erb :numbers
+  else
 	" In 10 years you will be: #{params[:howold].to_i + 10}."
+  end
 end
+
+post '/numbers' do
+  favnum1 = params['favnum1'].to_i
+  favnum2 = params['favnum2'].to_i
+  favnem3 = params['favnum3'].to_i
+
+  sum = favnum1 + favnum2 + favnem3
+  "total of your 3 fav numbers is #{sum}"
+end
+
+
